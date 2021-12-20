fc090d5e6072d78e47b7c53df37fbf441e29eea0
Kshipra Jadav <20bt04013@gsfcuniversity.ac.in>
2021-12-20 22:49:54 +0530

    Added TCP functionality. The Server can now actively listen to TCP connections

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,6 @@
+use std::net::TcpListener;
+use std::io::Read;
+
 pub struct Server{
     address : String
 }
@@ -11,6 +14,31 @@ impl Server{
 
     pub fn run(self){
         println!("Server running on : {}", self.address);
+        let listener = TcpListener::bind(&self.address).unwrap();
+
+
+        loop {
+            match listener.accept(){
+    
+                Ok((mut stream, addr)) => {
+                    println!("Connected : address : {}", addr);
+                    // println!("Stream : {:?}", stream);
+
+                    // for stream
+
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer){
+                        Ok(_) => println!("Request Recieved : {}", String::from_utf8_lossy(&buffer)),
+                        Err(e) => println!("There is some error in reading : {}", e)
+                    }
+                },
+                Err(err) => println!("Error! {}", err)
+    
+            }
+
+        }
+
+        
         
     }
 }
